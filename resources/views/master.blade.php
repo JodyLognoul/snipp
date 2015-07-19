81062861e8219589e78249db81232fd82e007fbd
Jody Lognoul <lognoulj@gmail.com>
2015-07-19 14:04:50 +0200

    Add favicon

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>Snipp</title>
-
+	<link rel="icon" href="/favicon.ico" type="image/x-icon">
     <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="/css/app.css">
     
