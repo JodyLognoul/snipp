04ff8af58d95269d0e99647a6850b742ce44354c
Jody Lognoul <lognoulj@gmail.com>
2015-07-20 13:23:54 +0200

    Change the content field in table Snippet string => text

--- database/migrations/2015_07_19_165025_create_snippet_table.php
+++ database/migrations/2015_07_19_165025_create_snippet_table.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
 class CreateSnippetTable extends Migration
 {
@@ -15,7 +15,7 @@ class CreateSnippetTable extends Migration
         Schema::create('snippets', function (Blueprint $table) {
             $table->increments('id');
             $table->string('description');
-            $table->string('content');
+            $table->text('content');
             $table->boolean('public');
             $table->timestamps();
         });
