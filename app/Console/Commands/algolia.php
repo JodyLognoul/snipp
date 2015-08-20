9618102d567e16b5b5dde66d8a0f24c6dd7aa25a
Jody Lognoul <lognoulj@gmail.com>
2015-08-20 08:57:13 +0200

    Renamming Algolia command

--- app/Console/Commands/algolia.php
+++ app/Console/Commands/algolia.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Snippet;
 
-class algolia extends Command
+class Algolia extends Command
 {
     /**
      * The name and signature of the console command.
