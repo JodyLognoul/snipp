e5512cf7399ddb16e03bad45d4ebfbfa2409bf2c
Jody Lognoul <lognoulj@gmail.com>
2015-08-21 17:55:28 +0200

    Rename Algolia command

--- app/Console/Commands/AlgoliaCommand.php
+++ app/Console/Commands/AlgoliaCommand.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Snippet;
 
-class Algolia extends Command
+class AlgoliaCommand extends Command
 {
     /**
      * The name and signature of the console command.
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,9 +2,9 @@
 
 namespace App\Console;
 
+use App\Console\Commands\AlgoliaCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-
 
 class Kernel extends ConsoleKernel
 {
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        \App\Console\Commands\Algolia::class,
+        AlgoliaCommand::class,
     ];
 
     /**
